abdee6e999891f298bb16d26ebac21e752fa7f2e
Momen Abd El-Kader <moamenabdo2004@gmail.com>
2025-02-27 13:00:59 +0200

    feat: set-up server and client handler

---
+++ src/ChatServer.java
@@ -0,0 +1,90 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+
+public class ChatServer {
+
+    private final ServerSocket listener;
+    private final static int PORT = 8080;
+    private LinkedList<ClientHandler> clients = new LinkedList<>();
+    private volatile boolean is_running = true;
+
+    private final static PrintStream log = new PrintStream(System.out);
+    private final static PrintStream err_log = new PrintStream(System.err);
+
+    public ChatServer() throws IOException {
+        this(PORT);
+    }
+
+    public ChatServer(int port) throws IOException {
+        listener = new ServerSocket(port);
+    }
+
+    public void start() {
+        log.println("[SERVER] Server started on port " + listener.getLocalPort());
+        log.println("[SERVER] Press Ctrl+C to stop the server");
+
+        try {
+            while (is_running && !listener.isClosed()) {
+                Socket pending_client = listener.accept(); // wait for a client to connect to the port
+                log.println("[SERVER] A new client has connected.");
+                ClientHandler new_client = new ClientHandler(pending_client, this);
+                addClient(new_client);
+                // start a new thread for each client
+                Thread client_thread = new Thread(new_client);
+                client_thread.start();
+            }
+        }
+        catch (IOException exception) {
+            err_log.println("[SERVER] An error occurred: closing server socket... ");
+            close();
+        }
+    }
+
+    public void close() {
+        is_running = false;
+        try {
+            if (listener != null) {
+                listener.close();
+            }
+        }
+        catch (IOException exception)  {
+            err_log.println("[SERVER] Could not close server socket:  " + exception.getMessage());
+        }
+    }
+
+    public synchronized void broadcastMessage(String message, ClientHandler sender) {
+        log.println(message);
+        for (ClientHandler client : clients) {
+            if (client != sender) {
+                client.sendMessage(message);
+            }
+        }
+    }
+
+    public synchronized void removeClient(ClientHandler client) {
+        if (clients.contains(client)) {
+            broadcastMessage(client.getClientUsername() + " has left the chat.", null);
+            log.println("[SERVER] Client " + client.getClientUsername() + " has left the chat.");
+            clients.remove(client);
+            log.println("[SERVER] Active clients: " + clients.size());
+        }
+    }
+
+    public synchronized void addClient(ClientHandler client) {
+        clients.add(client);
+        log.println("[SERVER] Active clients: " + clients.size());
+    }
+
+    public static void main(String[] args)  {
+        try {
+            ChatServer server = new ChatServer();
+            server.start();
+        }
+        catch (IOException exception) {
+            err_log.println("[SERVER] Failed to start server: " + exception.getMessage());
+            System.exit(1);
+        }
+    }
+}
---
+++ src/ClientHandler.java
@@ -0,0 +1,97 @@
+import java.io.*;
+import java.net.Socket;
+
+import static java.lang.System.exit;
+
+
+public class ClientHandler implements Runnable {
+
+    private Socket client;
+    private ChatServer server;
+    private BufferedReader input;
+    private BufferedWriter output;
+    private String client_username;
+    private volatile boolean is_running = true;
+    private final static PrintStream err_log = new PrintStream(System.err);
+
+    ClientHandler(Socket client, ChatServer server) {
+        try {
+            this.client = client;
+            this.server = server;
+            this.input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            this.output = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+            client_username = input.readLine();
+            server.broadcastMessage(client_username + " has joined the chat.", this);
+        }
+        catch (IOException exception) {
+            err_log.println("[ClientHandler] An error occurred while initializing client: " + exception.getMessage());
+            closeConnection();
+        }
+    }
+
+    public void closeConnection() {
+        is_running = false;
+        try {
+            server.removeClient(this);
+
+            if (output != null) {
+                output.close();
+            }
+
+            if (input != null) {
+                input.close();
+            }
+
+            if (client != null) {
+                client.close();
+            }
+
+        }
+        catch (IOException exception) {
+            err_log.println("[ClientHandler] An error occurred while closing client socket: " + exception.getMessage());
+        }
+    }
+
+    public void sendMessage(String message) {
+        try {
+            if (output != null && is_running) {
+                output.write(message);
+                output.newLine();
+                output.flush();
+            }
+        }
+        catch (IOException exception) {
+            err_log.println("[ClientHandler] An error occurred while sending message: " + exception.getMessage());
+            closeConnection();
+        }
+    }
+
+    public String getClientUsername() {
+        return client_username;
+    }
+
+    @Override
+    public void run() {
+        try {
+            String message;
+            while (is_running && (message = input.readLine()) != null) {
+
+                if (message.trim().equalsIgnoreCase("/exit")) {
+                    break;
+                }
+
+                if (!message.trim().isEmpty()) {
+                    server.broadcastMessage(client_username + ": " + message, this);
+                }
+            }
+        }
+        catch (IOException exception) {
+            if (is_running) {
+                err_log.println("[ClientHandler] An error occurred while reading message: " + exception.getMessage());
+            }
+        }
+        finally {
+            closeConnection();
+        }
+    }
+}
